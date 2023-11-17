3f856c98ef836119d5a848da1a5f94f050ef5b2f
Santiago Figueroa <davidsfigueroar@outlook.es>
2023-11-17 18:59:23 -0500

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,101 @@
-import React, { useState } from 'react';
-import Admin from './pages/admin'; 
+import './App.css'
+import Card from './components/cards/cards';
+import data from './mooks/data.json'
 
 
 function App() {
   return (
-    <Admin />
+    <main style={{ display: "flex", justifyContent: "space-evenly", alignItems: "center", border: "0.5px solid black" }}>
+      <div className="col">
+        <section>
+          {data.map((i) => {
+            if (i.nivel === 1) {
+              return (
+                  <Card info={i.perfil} handle={true}></Card>
+              )
+            }
+          })}
+        </section>
+        <footer className="col-footer">
+          1
+        </footer>
+      </div>
+      <div className="col">
+      <section>
+          {data.map((i) => {
+            if (i.nivel === 2) {
+              return (
+                  <Card info={i.perfil} handle={true}></Card>
+              )
+            }
+          })}
+        </section>
+        <footer className="col-footer">
+          2
+        </footer>
+      </div>
+      <div className="col">
+      <section>
+          {data.map((i) => {
+            if (i.nivel === 3) {
+              return (
+                  <Card info={i.perfil} handle={true}></Card>
+              )
+            }
+          })}
+        </section>
+        <footer className="col-footer">
+          3
+        </footer>
+      </div>
+      <div className="col">
+      <section>
+          {data.map((i) => {
+            if (i.nivel === 4) {
+              return (
+                  <Card info={i.perfil} handle={true}></Card>
+              )
+            }
+          })}
+        </section>
+        <footer className="col-footer">
+          4
+        </footer>
+      </div>
+      <div className="col">
+      <section>
+          {data.map((i) => {
+            if (i.nivel === 5) {
+              return (
+                  <Card info={i.perfil} handle={true}></Card>
+              )
+            }
+          })}
+        </section>
+        <footer className="col-footer">
+          5
+        </footer>
+      </div>
+      <div className="col">
+      <section>
+          {data.map((i) => {
+            if (i.nivel === 6) {
+              return (
+                  <Card info={i.perfil} handle={true}></Card>
+              )
+            }
+          })}
+        </section>
+        <footer className="col-footer">
+          6
+        </footer>
+      </div>
+    </main>
+
+
   )
 }
 
 
 export default App;
+
